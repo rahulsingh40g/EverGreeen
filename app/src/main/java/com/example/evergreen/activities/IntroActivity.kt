2b38b37a1b3fd7746d5abd9a61c1d41d2c8942c2
rahul <rahulsingh40g@gmail.com>
2021-04-16 11:58:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/evergreen/activities/IntroActivity.kt
@@ -0,0 +1,18 @@
+package com.example.evergreen.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.evergreen.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
